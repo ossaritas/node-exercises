77c5d80cea645e628a8910dac0fe06e097181832
ossaritas <81383004+ossaritas@users.noreply.github.com>
2022-06-04 15:36:32 +0300

    some improvements - section 4

--- app.js
+++ app.js
@@ -1,36 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const routes = require('./routes');
 
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Welcome my first Node.js server</title></head>');
-    res.write(
-      '<body><form action="/create-user" method="POST"><input type="text" name="username"/><button type="submit">Send</button></form></body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/users' && method === 'GET') {
-    return res.end('<html><ul><li>User 1</li></ul></html>');
-  }
-  if (url === '/create-user' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const user = parsedBody.split('=')[1];
-      console.log(user);
-      res.statusCode = 302;
-      res.setHeader('Location', '/');
-      return res.end();
-    });
-  }
-});
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
