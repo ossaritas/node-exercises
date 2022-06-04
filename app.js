55c72edca0b129f5c6d228c0e15faa78478bb98a
ossaritas <81383004+ossaritas@users.noreply.github.com>
2022-06-04 21:02:29 +0300

    until creating html pages

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  console.log('In the middleware');
-  next();
-});
-app.use((req, res, next) => {
-  console.log('In another middleware');
+  res.status(404).send('<h1> Page Not Fount</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
